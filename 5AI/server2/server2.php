884fa056c1aabd6e35b20ea7c0c6758fae98dc21
Belloni Andrea <andreabelloniprbg@gmail.com>
2026-01-23 10:43:58 +0100

    Refactor server2.php with consistent formatting

--- 5AI/server2/server2.php
+++ 5AI/server2/server2.php
@@ -8,12 +8,13 @@ function conversione($lunghezza, $scala){
     $xml = simplexml_load_file($fileXML); //caricamento del file xml dal percorso specificato sopra
 
     foreach($xml->conversione){
-
+        
     }
 }
 
 $server= new SoapServer("prova.wsdl"); //creazione server con file prova.wsdl, il file definisce i metodi che il server può usare
 $server->addFunction("conversione"); //aggiunta della funzione conversione al server
 $server->handle(); //gestisce la richiesta
+
 
 ?>

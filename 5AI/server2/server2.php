4f5f3fab674cc529cde68dd0edfe93fe8e0c000c
Belloni Andrea <andreabelloniprbg@gmail.com>
2026-01-27 17:01:39 +0100

    Update server2.php

--- 5AI/server2/server2.php
+++ 5AI/server2/server2.php
@@ -4,17 +4,20 @@
 
 function conversione($lunghezza, $scala){
 
-    $fileXML="conversione.xml"
+    $fileXML="conversione.xml";
     $xml = simplexml_load_file($fileXML); //caricamento del file xml dal percorso specificato sopra
 
-    foreach($xml->conversione){
+    foreach($xml->conversioni[0]->conversione as $a){ //entri nel primo contenitore e iteri tutti i suoi elementi
         
+        if($scala==$a['from']){ //valuta che il $scala sia uguale all'attributo from dell'xml
+            $conversione= $lunghezza* $a['factor']; //moltiplica la lunghezza per il fattore di conversione trovato col ciclo
+        }
     }
+    return $conversione;
 }
 
 $server= new SoapServer("prova.wsdl"); //creazione server con file prova.wsdl, il file definisce i metodi che il server può usare
 $server->addFunction("conversione"); //aggiunta della funzione conversione al server
 $server->handle(); //gestisce la richiesta
-
 
 ?>
